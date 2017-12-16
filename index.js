3546d5f8a727070c81bcc4c222957fc12f383453
ramishalah <romioo1010@gmail.com>
2017-12-16 15:22:39 +0300

    adding some comment to the index file

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 
+// connecting to the clear db database
 var con = mysql.createPool({
   connectionLimit : 10,
   host: "us-cdbr-iron-east-05.cleardb.net",
@@ -14,12 +15,21 @@ var con = mysql.createPool({
 
 
 express()
+  // to parse the request body
   .use(bodyParser.json())
+
+  // for retrieving all the students
   .get('/students', function(req, res, next){
+
       var sql = "SELECT * FROM student";
       con.query(sql, function (err, rows, fields) {
         if (err) throw err;
         res.send(rows);
       });
+
     })
+
+
+
+
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))

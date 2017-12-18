2e93b3d40e6ba9e71148a26c5f5fdd1081aa0876
ramishalah <romioo1010@gmail.com>
2017-12-18 22:26:27 +0300

    adding req5

--- index.js
+++ index.js
@@ -112,93 +112,62 @@ express()
   // for retrieving the students who eligible to take a specific course
   .get('/:CourseCode', function(req, res, next){
 
+    
+
+    var prerequisite = [];
+    var studentsWithCourses = [];
+
     var prerequisiteSql = `select p.PreCourseCode
     from course c join prerequisite p
     on c.CourseCode = p.CourseCode
     where c.CourseCode = '${req.params.CourseCode}'`;
 
 
-    var prerequisite = [];
     con.query(prerequisiteSql, function (err, rows, fields) {
       if (err) throw err;
       prerequisite = rows;
-      res.send(prerequisite);
-    });
+      console.log(prerequisite);
 
-    var studentWithCoursesSql = `select s.StuID, s.Fname, s.Lname, se.CourseCode
-    from student s
-    join enrollment e
-    on s.StuID = e.StuID
-    join section se
-    on e.CRN = se.CRN`;
+      var studentWithCoursesSql = `select s.StuID, s.Fname, s.Lname, se.CourseCode
+      from student s
+      join enrollment e
+      on s.StuID = e.StuID
+      join section se
+      on e.CRN = se.CRN`;
 
-    var studentsWithCourses = [];
-    con.query(studentWithCoursesSql, function (err, rows, fields) {
-      if (err) throw err;
-      studentsWithCourses = rows;
-    });
-
-    // var students = [];
-
-    // var prerequisiteCourseCode = prerequisite[0].PreCourseCode;
-
-    // for(j = 0; j < studentsWithCourses.length; j++) {
-
-    //   var CourseCode = studentsWithCourses[j].CourseCode;
-    //   // var studentID1 = studentsWithCourses[j].StuID;
-    //   // var studentID2;
-    //   // if(studentsWithCourses[i + 1] != null)
-    //   //   studentID2 = studentsWithCourses[j + 1].StuID;
-
-    //   if(CourseCode == prerequisiteCourseCode) {
-    //     students.push(
-    //       {
-    //         StudID: studentsWithCourses[j].StuID,
-    //         Fname: studentsWithCourses[j].Fname,
-    //         Lname: studentsWithCourses[j].Lname
-    //       }
-    //     )
-    //   } 
-    // }
-    // res.send(students);
-
-    // var eachStudentWithCourses = [];
-    // var coursesArray = []
-    // for(i = 0; i < studentsWithCourses.length; i++) {
-    //   var studentID1 = studentsWithCourses[i].StuID;
-    //   var studentID2;
-    //   if(studentsWithCourses[i + 1] != null)
-    //     studentID2 = studentsWithCourses[i + 1].StuID;
-
-    //   coursesArray.push(studentsWithCourses.CourseCode);
       
-    //   if(studentID1 != studentID2) {
-    //     var studentHasCourses = {
-    //       studentID1: coursesArray
-    //     }
-    //     coursesArray = [];
-    //     eachStudentWithCourses.push(studentHasCourses);
+      con.query(studentWithCoursesSql, function (err, rows, fields) {
+        if (err) throw err;
+        studentsWithCourses = rows;
+        console.log(studentsWithCourses)
 
-    //   }
-    // }
+        var students = [];
+        for(j = 0; j < studentsWithCourses.length; j++) {
 
-    // var students = [];
-    // for(i = 0; i < prerequisite.length; i++) {
-    //   var preCourseCode = prerequisite[i].CourseCode;
-    //   for(j = 0; j < eachStudentWithCourses.length; j++){
-    //     var studentId = eachStudentWithCourses[j];
-    //     for(k = 0; k < eachStudentWithCourses.studentID1.length; k++) {
-    //       if(eachStudentWithCourses.studentID1[k] == preCourseCode)
-    //         students.push({
-    //           StuID: studentId,
+          if(prerequisite.length == 0) {
+            // There is a bug here!
+            students.push(
+              {
+                StudID: studentsWithCourses[j].StuID,
+                Fname: studentsWithCourses[j].Fname,
+                Lname: studentsWithCourses[j].Lname
+              }
+            )
+          } else if(studentsWithCourses[j].CourseCode == prerequisite[0].PreCourseCode && prerequisite.length != 0) {
+            students.push(
+              {
+                StudID: studentsWithCourses[j].StuID,
+                Fname: studentsWithCourses[j].Fname,
+                Lname: studentsWithCourses[j].Lname
+              }
+            )
+          }
+        }
+        res.send(students);
+        
+      });
 
-    //         });
-    //     }
-    //   }
-    // }
-
-
-
+    });
   })
 
   // for retrieving the id's and the names for all instructors who can teach a specific course
